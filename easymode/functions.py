cbd017a2da65c0086bd493aea57607a31dafec68
Huriel Romanzini <chuva_de_pedra@hotmail.com>
2021-11-25 17:47:38 -0300

    consertado funcoes

--- easymode/functions.py
+++ easymode/functions.py
@@ -11,43 +11,43 @@ def decforhex(number):
     return numbconv
     
 def binfordec(number):
-    numbconv = eval('0b' + number)
+    numbconv = eval('0b' + str(number))
     return numbconv
 
 def binforoct(number):
-    dec = eval('0b' + number)
+    dec = eval('0b' + str(number))
     numbconv = decforoct(dec)
     return numbconv
 
 def binforhex(number):
-    dec = eval('0b' + number)
+    dec = eval('0b' + str(number))
     numbconv = decforhex(dec)
     return numbconv
 
 def octfordec(number):
-    numbconv = eval('0o' + number)
+    numbconv = eval('0o' + str(number))
     return numbconv
 
 def octforbin(number):
-    dec = decforbin('0o' + number)
+    dec = eval('0o' + str(number))
     numbconv = decforbin(dec)
     return numbconv
 
 def octforhex(number):
-    dec = eval('0o' + number)
-    numbconv = devforhex(dec)
+    dec = eval('0o' + str(number))
+    numbconv = decforhex(dec)
     return numbconv
-
+#usar input entre aspas para converter de hexadecimal para outras bases
 def hexfordec(number):
-    numbconv = eval('0x' + number)
+    numbconv = eval('0x' + str(number))
     return numbconv
 
 def hexforbin(number):
-    dec = eval('0x' + number)
+    dec = eval('0x' + str(number))
     numbconv = decforbin(dec)
     return numbconv
 
 def hexforoct(number):
-    dec = eval('0x' +number)
+    dec = eval('0x' + str(number))
     numbconf = decforoct(dec)
     return numbconf
